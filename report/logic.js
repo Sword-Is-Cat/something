ada331dc6675d920b1a716a1747cf558d7dd12cb
Jang hongbum <62692449+Sword-Is-Cat@users.noreply.github.com>
2022-08-06 21:17:46 +0900

    rpt udt
    1234

--- report/logic.js
+++ report/logic.js
@@ -9,18 +9,21 @@ function addRow(){
 		<li>
 			<input type='text'/>
 			<br/>
+			항마력
 			<label><input type='radio' name='power${idx}'>16000</input></label>
 			<label><input type='radio' name='power${idx}'>17000</input></label>
 			<label><input type='radio' name='power${idx}'>18000</input></label>
 			<label><input type='radio' name='power${idx}'>19000</input></label>
 			<label><input type='radio' name='power${idx}'>20000</input></label>
 			<br/>
+			전시즌
 			<label><input type='radio' name='prerank${idx}'>달인</input></label>
 			<label><input type='radio' name='prerank${idx}'>명인</input></label>
 			<label><input type='radio' name='prerank${idx}'>소패</input></label>
 			<label><input type='radio' name='prerank${idx}'>패왕</input></label>
 			<label><input type='radio' name='prerank${idx}'>투신</input></label>
 			<br/>
+			현시즌
 			<label><input type='radio' name='currank${idx}'>달인</input></label>
 			<label><input type='radio' name='currank${idx}'>명인</input></label>
 			<label><input type='radio' name='currank${idx}'>소패</input></label>
@@ -30,4 +33,31 @@ function addRow(){
 	
 	$('#inputSection').append(html);
 	
+}
+
+function download(){
+	
+	let data = '';
+	
+	$('li').each(function(element){
+		
+		console.log($(element).child('input:text').val());
+		console.log($(element).child('input:checked').val());
+		
+		data += ``;
+		
+	});
+	
+	/*
+    let element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(codes.homecode + codes.datacode));
+    element.setAttribute('download', 'save.dat');
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+	*/
 }
