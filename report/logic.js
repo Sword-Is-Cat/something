140e3641c8e09db47779797a683dcb8e188d8ab4
Jang hongbum <62692449+Sword-Is-Cat@users.noreply.github.com>
2022-08-06 21:46:30 +0900

    ㅁㄴㅇㄹ
    ㅁㄴㅇㄹ

--- report/logic.js
+++ report/logic.js
@@ -1,34 +1,38 @@
 let idx = 0;
+let data = new Object();
+let text;
 
 function addRow(){
 	
 	let html;
 	idx++;
 	
+	data['mem'+idx] = new Object();
+	
 	html = `
-		<li>
-			<input type='text'/>
+		<li id='mem${idx}'>
+			<input type='text'/ oninput='texting(this)'>
 			<br/>
 			항마력
-			<label><input type='radio' name='power${idx}'>16000</input></label>
-			<label><input type='radio' name='power${idx}'>17000</input></label>
-			<label><input type='radio' name='power${idx}'>18000</input></label>
-			<label><input type='radio' name='power${idx}'>19000</input></label>
-			<label><input type='radio' name='power${idx}'>20000</input></label>
+			<label><input type='radio' name='power${idx}' key='power' onchange='radioChng(this)' value='16000'>16000</input></label>
+			<label><input type='radio' name='power${idx}' key='power' onchange='radioChng(this)' value='17000'>17000</input></label>
+			<label><input type='radio' name='power${idx}' key='power' onchange='radioChng(this)' value='18000'>18000</input></label>
+			<label><input type='radio' name='power${idx}' key='power' onchange='radioChng(this)' value='19000'>19000</input></label>
+			<label><input type='radio' name='power${idx}' key='power' onchange='radioChng(this)' value='20000'>20000</input></label>
 			<br/>
 			전시즌
-			<label><input type='radio' name='prerank${idx}'>달인</input></label>
-			<label><input type='radio' name='prerank${idx}'>명인</input></label>
-			<label><input type='radio' name='prerank${idx}'>소패</input></label>
-			<label><input type='radio' name='prerank${idx}'>패왕</input></label>
-			<label><input type='radio' name='prerank${idx}'>투신</input></label>
+			<label><input type='radio' name='prerank${idx}' key='prerank' onchange='radioChng(this)' value='달인'>달인</input></label>
+			<label><input type='radio' name='prerank${idx}' key='prerank' onchange='radioChng(this)' value='명인'>명인</input></label>
+			<label><input type='radio' name='prerank${idx}' key='prerank' onchange='radioChng(this)' value='소패'>소패</input></label>
+			<label><input type='radio' name='prerank${idx}' key='prerank' onchange='radioChng(this)' value='패왕'>패왕</input></label>
+			<label><input type='radio' name='prerank${idx}' key='prerank' onchange='radioChng(this)' value='투신'>투신</input></label>
 			<br/>
 			현시즌
-			<label><input type='radio' name='currank${idx}'>달인</input></label>
-			<label><input type='radio' name='currank${idx}'>명인</input></label>
-			<label><input type='radio' name='currank${idx}'>소패</input></label>
-			<label><input type='radio' name='currank${idx}'>패왕</input></label>
-			<label><input type='radio' name='currank${idx}'>투신</input></label>
+			<label><input type='radio' name='currank${idx}' key='currank' onchange='radioChng(this)' value='달인'>달인</input></label>
+			<label><input type='radio' name='currank${idx}' key='currank' onchange='radioChng(this)' value='명인'>명인</input></label>
+			<label><input type='radio' name='currank${idx}' key='currank' onchange='radioChng(this)' value='소패'>소패</input></label>
+			<label><input type='radio' name='currank${idx}' key='currank' onchange='radioChng(this)' value='패왕'>패왕</input></label>
+			<label><input type='radio' name='currank${idx}' key='currank' onchange='radioChng(this)' value='투신'>투신</input></label>
 		</li>`;
 	
 	$('#inputSection').append(html);
@@ -37,27 +41,38 @@ function addRow(){
 
 function download(){
 	
-	let data = '';
+	text = '';
 	
-	$('li').each(function(element){
-		
-		console.log($(element).child('input:text').val());
-		console.log($(element).child('input:checked').val());
-		
-		data += ``;
-		
+	Object.keys(data).forEach(function(rowname){
+		let row = data[rowname];
+		if(text!=='')
+			text+='\n';
+		text += row.name+" / "+row.power+" / 전: "+(row.prerank||'-')+" / 현: "+(row.currank||'-');
 	});
 	
-	/*
-    let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(codes.homecode + codes.datacode));
-    element.setAttribute('download', 'save.dat');
+	
+	 let element = document.createElement('a'); 
+	 element.setAttribute('href','data:text/plain;charset=utf-8,' + encodeURIComponent(text)); 
+	 element.setAttribute('download', 'result.txt');
+	  
+	  element.style.display = 'none'; document.body.appendChild(element);
+	 
+	  element.click();
+	  
+	  document.body.removeChild(element);
+	 
+}
 
-    element.style.display = 'none';
-    document.body.appendChild(element);
+function radioChng(oThis){
+	let key = $(oThis).attr('key');
+	let val = $(oThis).val();
+	let id = $(oThis).parent().parent().attr('id');
+	data[id][key] = val;
+}
 
-    element.click();
-
-    document.body.removeChild(element);
-	*/
+function texting(oThis){
+	
+	let id = $(oThis).parent().attr('id');
+	data[id].name = $(oThis).val();
+	
 }
