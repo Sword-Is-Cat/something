43570896dc48207f343c65a6bfdda4e7ae5e1977
jang <jhbsp@naver.com>
2022-09-06 16:39:29 +0900

    220906

--- resume/logic.js
+++ resume/logic.js
@@ -3,7 +3,6 @@
  */
 const copy = (text) => {
 	
-	console.log('copy: '+text);
   // 임시의 textarea 생성
   const $textarea = document.createElement("textarea");
 
@@ -18,15 +17,64 @@ const copy = (text) => {
   document.execCommand('copy');
   document.body.removeChild($textarea);
   
-  alert('copied!');
+  alert('복사되었습니다');
 }
 
 const btn_click = () =>{
 	
-	let text = '${name} \n ${job} \n ${power}';
+	let text = 
+		'${name} / ${job} / ${power}';
 	
-	console.log('btnC: '+text);
+	let name = document.getElementById('name').value;
+	let job = document.getElementById('job_2').value;
+	let power = document.getElementById('power').value;
 	
-	copy(text);
+	if(name && job && power && job!='null'){
+		
+		text = text.replace('${name}', name).replace('${job}', job).replace('${power}', power);
+		copy(text);
+		
+	}else{
+		
+		alert('입력이 부족합니다');
+		
+	}
+	
+	
+	
+}
+
+const jobChange = (value) => {
+	
+	console.log('jc:'+value);
+	
+	let $select = document.getElementById('job_2');
+	let $jobName = document.getElementById('job');
+	
+	if(!value){
+		
+		$select = document.getElementById('job_1');
+		
+		while($select.options.length>0)
+			$select.remove(0);
+		
+		//$select.add(new Option('-'));
+		
+		jobs.all.forEach(arr => {
+			$select.add(new Option(arr[1], arr[0]));
+		});
+		
+	}else if(Object.keys(jobs).indexOf(value)!=-1){
+		
+		while($select.options.length>0)
+			$select.remove(0);
+		
+		$select.add(new Option('-', null));
+		
+		jobs[value].forEach(job => {
+			$select.add(new Option(job));
+		});
+		
+	}
 	
 }
