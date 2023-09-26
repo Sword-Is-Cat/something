776cba48e131432762fd60a65feec28187b20198
jang <jhbsp@naver.com>
2023-09-26 11:20:19 +0900

    230926 inputForm

--- rynoCalc/logic.js
+++ rynoCalc/logic.js
@@ -1,12 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    submitBtnEvent("submit");
+    document.getElementById("inputLevel").value = defaultLv;
+    document.getElementById("inputSkill").value = defaultSp;
     drawTable();
 
 });
 
+const defaultLv = 40;
+const defaultSp = 350;
+
+const submitBtnEvent = (btnId) => {
+    const btn = document.getElementById(btnId);
+    btn.addEventListener("click", () => {
+        const lvVal = document.getElementById("inputLevel").value;
+        const spVal = document.getElementById("inputSkill").value;
+        if(isNaN(Number(lvVal))){
+            lvVal = defaultLv;
+            document.getElementById("inputLevel").value = defaultLv;
+        }
+        if(isNaN(Number(spVal))){
+            spVal = defaultSp;
+            document.getElementById("inputSkill").value = defaultSp;
+        }
+        if(spVal%5>0){
+            spVal -= spVal%5;
+            document.getElementById("inputSkill").value = spVal;
+        }
+        drawTable(Number(lvVal), Number(spVal));
+    });
+}
+
 const drawTable = (st_lv, st_sp) => {
-    st_lv = st_lv || 40;
-    st_sp = st_sp || 350;
+    st_lv = st_lv || defaultLv;
+    st_sp = st_sp || defaultSp;
     const step = 5;
     /** table 초기화 */
     const $table = document.getElementById("dataTable");
